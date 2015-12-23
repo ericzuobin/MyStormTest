bee7c251ea8ee9c5f6c8b3df4b407371ce531104
sahinn <=>
2015-12-23 15:14:05 +0800

    删除main方法

--- src/main/java/net/zuobin/storm/spout/IpSpout.java
+++ src/main/java/net/zuobin/storm/spout/IpSpout.java
@@ -8,8 +8,6 @@ import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -45,20 +43,4 @@ public class IpSpout extends BaseRichSpout{
             _outOutputCollector.emit(new Values(s));
         }
     }
-
-    public static void main(String[] args) throws Exception{
-
-        Process process = null;
-        List<String> processList = new ArrayList<String>();
-        process = Runtime.getRuntime().exec("ssh sahinn@172.16.22.250 /usr/java/jdk1.7.0_79/bin/jps | grep \"\\(nimbus\\|core\\|drpc\\|supervisor\\|logviewer\\)\" | awk '{print $1 \"|\" $2 }'");
-        BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line = "";
-        while ((line = input.readLine()) != null) {
-            processList.add(line);
-        }
-        input.close();
-
-        for (String li : processList) {
-            System.out.println(li);
-        }     }
 }
